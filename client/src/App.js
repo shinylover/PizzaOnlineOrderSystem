e0d7faccdd55ede3623189174c042ec0bee90345
shinylover <shinylover520@gmail.com>
2021-01-15 04:21:28 +0800

    build the structure of the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Redirect, Route, Link } from 'react-router-dom';
+import {Switch} from 'react-router';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from './logo.svg';
+import '../src/styles/App.css';
+import { AuthContext } from './auth/AuthContext';
+
+class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  
+  render() {
+
+    const value = {
+      authUser: this.state.authUser,
+      authErr: this.state.authErr,
+      loginUser: this.login,
+      logoutUser: this.logout
+    }
+    
+    return (
+      <AuthContext.Provider value={value}>
+        <Header>
+          Nothing
+        </Header>
+        <Router>
+          <Switch>
+            <Route exact path='/Login'>
+              {/* TODO: loginPage */}
+            </Route>
+            <Route path='/Login'>
+              {/* TODO: clientPage */}
+            </Route>
+            <Route  path='/Login'>
+              {/* TODO: shopperPage */}
+            </Route>
+            <Route exact path='/'>
+              <Redirect to= 'Login'/>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthContext.Provider>
+    )
+  }
 }
 
 export default App;
